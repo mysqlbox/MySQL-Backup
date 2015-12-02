7db0f39a8f39f38054c389a2c411c35f5b345b6f
MySQL Box <allan@mysqlbox.ml>
2015-12-02 00:07:32 -0200

    Versao 1.2

--- Backup_MySQL.sh
+++ Backup_MySQL.sh
@@ -2,45 +2,111 @@
 #
 # MySQL_Backup
 #
-# Copyright (C) 2015 Allan Moraes <allan@mysqlbox.ml> <www.mysqlbox.ml>
+# Copyright (C) 2015 Allan Moraes <allan@mysqlbox.com.br> <www.mysqlbox.com.br>
 #
-# Este e um software livre e gratuíto que atua com a licensa 
-# GNU GENERAL PUBLIC LICENSE Version 3. O seu uso e permitido
-# mas a sua venda e proibida.
+# Este programa e um software livre; voce pode redistribui-lo e / ou modifica-lo 
+# sob os termos da GNU General Public License como publicado pela
+# Free Software Foundation; tanto a versao 3 da Licenca, ou
+# (A seu criterio) qualquer versao posterior.
+#
+# Este programa a distribuido para ajudar todos DBA,
+# mas SEM QUALQUER GARANTIA; mesmo sem a garantia implicita de
+# COMERCIALIZACAO ou ADEQUACAO A UM DETERMINADO FIM. veja a
+# GNU General Public License para mais detalhes.
+#
+# Voce deve ter recebido uma copia da Licença Publica Geral GNU
+# junto com este programa; se nao, escreva para a Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, EUA.
 
-BACKUP_PATH="/usr/local/bin/MySQL-Backup/tmp" #Local temporario dos backups
-USER="" #Usuario do backup
-SECRET"" #Senha do usuario
-DROPBOX_PATH="/usr/local/bin/Dropbox-Uploader" #Local de instalacao do Dropbox-Uploader
-LOG="/var/log/mysql-backup.log" #Local dos logs
+###Parametros do script###
 
+BACKUP_NAME="backup-`hostname`-`date +%d_%m_%Y`"	#Nome que ficara o backup. Ex: backup-SERVER01-01_12_2015
+BACKUP_PATH="/usr/local/bin/MySQL-Backup"		#Local do diretorio do script
+BACKUP_TEMP="$BACKUP_PATH/tmp" 				#Local temporario dos backups
+BACKUP_SHELL="$BACKUP_PATH/Backup_MySQL.sh"		#Shell do backup
+PERMISSIONS=`stat -c %a $BACKUP_SHELL 2>&1`		#Pega as permissoes do shell
+DROPBOX_PATH="/usr/local/bin/Dropbox-Uploader" 		#Local de instalacao do Dropbox-Uploader
+DROPBOX_FILE="$DROPBOX_PATH/dropbox_uploader.sh"	#Local do Dropbox-Uploader.sh
+LOG_FILE="/var/log/mysql-backup.log" 			#Local dos logs
+USER="" 						#Usuario do backup
+SECRET="" 						#Senha do usuario
 
-#Funcao que consulta todos os bancos do seu servidor
+###Nao editar abaixo###
+
+#Verifica se o nome do backup nao e nulo
+if [ -z $BACKUP_NAME ]; then
+	echo -e "\n\tERRO: Variavel BACKUP_NAME contem valor nulo"
+	echo  "`date` - ERRO: Variavel BACKUP_NAME contem valor nulo" >> $LOG_FILE
+	#exit 1
+fi
+
+#Verifica se o local configurado esta correto
+if [ ! -d $BACKUP_PATH ]; then
+	echo -e "\n\tERRO: Variavel BACKUP_PATH esta incorreta, o local de instalacao nao existe"
+	echo "`date` - ERRO: Variavel BACKUP_PATH esta incorreta, o local de instalacao nao existe" >> $LOG_FILE
+	##exit 1
+fi
+
+#Verifica se o local temporario existe
+if [ ! -d $BACKUP_TEMP ]; then
+	echo -e "\n\tERRO: Variavel BACKUP_TEMP esta incorreta, o local de backup nao existe"
+	echo "`date` - ERRO: Variavel BACKUP_TEMP esta incorreta, o local de backup nao existe" >> $LOG_FILE
+	#exit 1
+fi
+if [ ! -f $BACKUP_SHELL ]; then
+	echo -e "\n\tERRO: O shell MySQL_Backup.sh nao foi encontrado"
+	echo "`date` - ERRO: O shell MySQL_Backup.sh nao foi encontrado" >> $LOG_FILE
+	#exit 1
+fi
+#Verifica se a permissao do shell esta correta
+if [ $PERMISSIONS != "700" ]; then
+	echo -e "\n\tERRO: Permissao do arquivo $BACKUP_SHELL incorreta! Permissao deve ser 700"
+	echo "`date` - ERRO: Permissao do arquivo $BACKUP_SHELL incorreta! Permissao deve ser 700" >> $LOG_FILE
+	#exit 1
+fi
+
+#Verifica se o local do Dropbox-Uploader esta correto
+if [ ! -f $DROPBOX_FILE ]; then
+	echo -e "\n\tERRO: Variavel DROPBOX_FILE esta incorreta, o local informado do Dropbox-Uploader nao existe"
+	echo "`date` - ERRO: Variavel DROPBOX_FILE esta incorreta, o local informado do Dropbox-Uploader nao existe" >> $LOG_FILE
+	#exit 1
+fi
+
+#Verifica se o usuario nao e nulo
+if [ -z $USER ]; then
+	echo -e "\n\tERRO: Usuario nao configurado"
+	echo "`date` - ERRO: Usuario nao configurado" >> $LOG_FILE
+	#exit 1
+fi
+
+#Verifica se a senha nao e nula
+if [ -z $SECRET ]; then
+	echo -e "\n\tERRO: A senha do usuario $USER nao foi fornecida"
+	echo "`date` - ERRO: A senha do usuario $USER nao foi fornecida" >> $LOG_FILE
+	#exit 1
+fi
+
+#Funcao que consulta todos os bancos do seu servidor e faz o backup
 function Get-Databases(){
-	if [ -d $BACKUP_PATH ]; then
-		echo "`date`  -  Path temporario OK" >> $LOG
-	else 
-		mkdir -p $BACKUP_PATH
-	fi
 	for DB in `mysql -u$USER -p$SECRET -e "SHOW DATABASES"|grep -v Database`; do
-		echo "`date`  -  Fazendo backup do banco $DB" >> $LOG
-		mysqldump -u$USER -p$SECRET $DB > $BACKUP_PATH/$DB.sql >> $LOG 2>&1
+		echo "`date`  -  Fazendo backup do banco $DB"
+		mysqldump -u$USER -p$SECRET  $DB > $BACKUP_TEMP/$DB.sql
 	done
 }
 
 #Funcao que compact dos backups
 function Zip-Databases(){
-	cd $BACKUP_PATH
-	tar -czf backup-`hostname`-`date +%d_%m_%Y`.tar.gz *.sql
+	cd $BACKUP_TEMP
+	tar -czf $BACKUP_NAME.tar.gz *.sql
 }
 
 #Funca que faz o upload do backup
 function Upload-Databases(){
-	$DROPBOX_PATH/dropbox_uploader.sh upload $BACKUP_PATH/*.tar.gz >> $LOG 2>&1
-	rm -rf $BACKUP_PATH/*
+	$DROPBOX_FILE upload $BACKUP_TEMP/*.tar.gz >> $LOG_FILE 2>&1
+	rm -rf $BACKUP_TEMP/*
 }
 
 #Chama as funcoes criadas acima
-Get-Databases
+Get-Databases >> $LOG_FILE 2>&1
 Zip-Databases
 Upload-Databases
